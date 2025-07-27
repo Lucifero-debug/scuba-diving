4e814c25d3b6a4dbbd25fdddb30e8349861f1300
Prashant <shishupal78008@gmail.com>
2025-07-27 13:07:08 +0530

    ddd

--- components/VideoGallery.jsx
+++ components/VideoGallery.jsx
@@ -30,28 +30,28 @@ const videos = [
     video: "https://media.istockphoto.com/id/1413729919/video/panoramic-aerial-drone-flight-video-around-trekker-on-green-hill-with-beautiful-sunset-scene.mp4",
   },
   {
-    thumbnail: "https://images.unsplash.com/photo-1537420327992-d6e192287183?w=600&auto=format&fit=crop&q=60",
-    video: "https://media.istockphoto.com/id/1413729919/video/panoramic-aerial-drone-flight-video-around-trekker-on-green-hill-with-beautiful-sunset-scene.mp4",
+    thumbnail: "https://images.unsplash.com/photo-1557235934-653f4f63c93e?q=80&w=626&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    video: "https://cdn.pixabay.com/video/2021/04/29/72566-543910236_tiny.mp4",
   },
   {
-    thumbnail: "https://images.unsplash.com/photo-1537420327992-d6e192287183?w=600&auto=format&fit=crop&q=60",
-    video: "https://media.istockphoto.com/id/1413729919/video/panoramic-aerial-drone-flight-video-around-trekker-on-green-hill-with-beautiful-sunset-scene.mp4",
+    thumbnail: "https://images.unsplash.com/photo-1649716729285-689bee1d47de?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    video: "https://cdn.pixabay.com/video/2025/04/09/270940_tiny.mp4",
   },
   {
-    thumbnail: "https://images.unsplash.com/photo-1537420327992-d6e192287183?w=600&auto=format&fit=crop&q=60",
-    video: "https://media.istockphoto.com/id/1413729919/video/panoramic-aerial-drone-flight-video-around-trekker-on-green-hill-with-beautiful-sunset-scene.mp4",
+    thumbnail: "https://images.unsplash.com/photo-1549983091-48c70653663b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fGFkdmVudHVyZSUyMGFjdGlpdml0aWVzfGVufDB8fDB8fHww",
+    video: "https://cdn.pixabay.com/video/2016/11/15/6387-191695740_tiny.mp4",
   },
   {
-    thumbnail: "https://images.unsplash.com/photo-1537420327992-d6e192287183?w=600&auto=format&fit=crop&q=60",
-    video: "https://media.istockphoto.com/id/1413729919/video/panoramic-aerial-drone-flight-video-around-trekker-on-green-hill-with-beautiful-sunset-scene.mp4",
+    thumbnail: "https://images.unsplash.com/photo-1556904576-61c4c0fd271b?q=80&w=626&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    video: "https://cdn.pixabay.com/video/2019/10/15/27948-366570969_tiny.mp4",
   },
   {
-    thumbnail: "https://images.unsplash.com/photo-1537420327992-d6e192287183?w=600&auto=format&fit=crop&q=60",
-    video: "https://media.istockphoto.com/id/1413729919/video/panoramic-aerial-drone-flight-video-around-trekker-on-green-hill-with-beautiful-sunset-scene.mp4",
+    thumbnail: "https://images.unsplash.com/photo-1627322183897-2de64b7f245e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    video: "https://cdn.pixabay.com/video/2015/08/12/368-136082428_tiny.mp4",
   },
   {
-    thumbnail: "https://images.unsplash.com/photo-1537420327992-d6e192287183?w=600&auto=format&fit=crop&q=60",
-    video: "https://media.istockphoto.com/id/1413729919/video/panoramic-aerial-drone-flight-video-around-trekker-on-green-hill-with-beautiful-sunset-scene.mp4",
+    thumbnail: "https://images.unsplash.com/photo-1637854087454-f0c80ab21999?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFkdmVudHVyZSUyMGFjdGlpdml0aWVzfGVufDB8fDB8fHww",
+    video: "https://www.istockphoto.com/video/speed-ramp-two-snowboarders-riding-towards-the-setting-sun-gm1139869862-304797632?utm_source=pixabay&utm_medium=affiliate&utm_campaign=sponsored_video&utm_content=srp_topbanner-test1_media&utm_term=adventure+activities",
   },
 ];
 
